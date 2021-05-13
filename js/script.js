c094f142b739d2430cc7019d6c7bbfbf97e160be
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-05-13 11:23:08 -0500

    node project added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+*This is to change the frosted window text from the original color 
+*to a given color for and it will stay for 3 seconds .
+*/
+setTimeout(() => {
+    let title = document.getElementById('frosted');
+    title.style.color = "#fff";
+    console.log(title);
+}, 3000);
+
+
+
+
